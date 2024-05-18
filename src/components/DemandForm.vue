16a791fd801068b6986c1b60c0ec799284f9050d
Sabrina <chiawenli.1219@gmail.com>
2024-05-18 23:49:53 +0800

    [MODIFY] basics

--- src/components/DemandForm.vue
+++ src/components/DemandForm.vue
@@ -1,53 +1,97 @@
 <template>
-  <el-button plain @click="dialogFormVisible = true"> 表單 </el-button>
-  <el-dialog v-model="dialogFormVisible" title="我要捐獻！" width="500">
-    <el-form :model="form">
-      <el-form-item label="姓名" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="單位名稱" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="聯絡電話" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="內容" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="份數" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="類型" :label-width="formLabelWidth">
-        <el-select v-model="form.region" placeholder="物資類型">
-          <el-option label="食物" value="shanghai" />
-          <el-option label="醫療" value="beijing" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="地點" :label-width="formLabelWidth">
-        <el-select v-model="form.region" placeholder="發放地點">
-          <el-option label="台中火車站" value="shanghai" />
-          <el-option label="光復地下道" value="beijing" />
-        </el-select>
-      </el-form-item>
-    </el-form>
+  <el-dialog v-model="visible" width="500">
+    <p style="font-size: 2em">我要捐獻！</p>
+    <div class="formContent">
+      <el-form :model="form" :label-position="'left'" style="padding: 15px">
+        <el-form-item label="姓名" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.name"
+            placeholder="請填寫姓名（例: 王小明、Andy-lee）"
+            autocomplete="off"
+          />
+        </el-form-item>
+        <el-form-item label="單位名稱" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.org"
+            placeholder="請填寫單位名稱（例: 街角家）"
+            autocomplete="off"
+          />
+        </el-form-item>
+        <el-form-item label="聯絡電話" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.phone"
+            placeholder="請填寫手機號碼（例：0912345678）"
+            autocomplete="off"
+          />
+        </el-form-item>
+        <el-form-item label="內容" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.content"
+            placeholder="請描述物資內容 （例：素菜便當）"
+            autocomplete="off"
+          />
+        </el-form-item>
+        <el-form-item label="份數" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.quantity"
+            placeholder="請填寫提供物資份數（例：10）"
+            autocomplete="off"
+          >
+            <template #append>份</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="類型" :label-width="formLabelWidth">
+          <el-select v-model="form.type" placeholder="請選擇物資類型">
+            <el-option label="食物" value="food" />
+            <el-option label="保暖" value="clothes" />
+            <el-option label="衛生" value="hygiene" />
+            <el-option label="醫療" value="medical" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="地點" :label-width="formLabelWidth">
+          <el-select v-model="form.location" placeholder="請選擇發放地點">
+            <el-option label="台中火車站" value="1" />
+            <el-option label="民權地下道" value="2" />
+            <el-option label="光復國小" value="3" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期" :label-width="formLabelWidth">
+          <el-date-picker v-model="date" type="date" placeholder="請選擇日期" />
+        </el-form-item>
+        <el-form-item label="時間" :label-width="formLabelWidth">
+          <el-time-select
+            v-model="time"
+            style="width: 240px"
+            start="07:00"
+            step="00:15"
+            end="22:30"
+            placeholder="請選擇時間"
+          />
+        </el-form-item>
+      </el-form>
+    </div>
     <template #footer>
       <div class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">
-          Confirm
-        </el-button>
+        <el-button @click="visible = false">取消</el-button>
+        <el-button type="primary" @click="visible = false"> 確認 </el-button>
       </div>
     </template>
   </el-dialog>
 </template>
 
-<script setup>
-import { reactive, ref } from "vue";
+<script lang="ts" setup>
+import { ref, watch } from "vue";
 
-const dialogFormVisible = ref(false);
-const formLabelWidth = "140px";
+const date = ref("");
+const time = ref("");
 
-const form = reactive({
+const props = defineProps({
+  visible: Boolean,
+});
+const visible = ref(props.visible);
+
+console.log("Visible prop:", visible.value);
+const form = ref({
   name: "",
   region: "",
   date1: "",
@@ -57,4 +101,18 @@ const form = reactive({
   resource: "",
   desc: "",
 });
+const formLabelWidth = "140px";
+
+watch(
+  () => props.visible,
+  (newValue) => {
+    visible.value = newValue;
+  }
+);
 </script>
+
+<style scoped>
+:deep .el-form-item__label {
+  font-size: 16px;
+}
+</style>
