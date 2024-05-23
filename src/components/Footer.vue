3756c4cf2d60a01447480c6497c861236af69ec4
Sabrina <chiawenli.1219@gmail.com>
2024-05-23 20:38:06 +0800

    [MODIFY]

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="footer-container bg-orange-50">
-    <div class="flex justify-center items-center px-4 py-8">
-      <!-- Left side: Image -->
-      <div class="flex-shrink-0">
-        <img class="h-80 w-auto" src="/img/banner.png" alt="Hero Image" />
-      </div>
+  <!-- <div class="footer-container bg-orange-50">
+    <div class="flex justify-evenly items-center px-4 py-8"> -->
+  <!-- Left side: Image -->
+  <!-- <div class="flex-shrink-0">
+        <img class="h-80 w-auto" src="/img/full-logo.png" alt="Footer Image" />
+      </div> -->
+  <!-- Mid side: Title -->
+  <!-- <div class="ml-4">
+        <h1 class="text-2xl font-bold text-black">街角家</h1>
+        <p class="text-lg text-gray-800">HomeSweetHome</p>
+        <p class="text-lg text-gray-800">聯繫我們</p>
+      </div> -->
 
-      <!-- Right side: Title -->
-      <div class="ml-4">
-        <h1 class="text-2xl font-bold text-black">Footer</h1>
-        <p class="text-lg text-gray-800">Footer</p>
+  <!-- Right side: Title -->
+  <!-- <div class="ml-4">
+        <h1 class="text-2xl font-bold text-black">捐款資訊</h1>
+        <p class="text-lg text-gray-800">XXXX-XXXX-XXXX-XXXX</p>
       </div>
     </div>
-  </div>
+  </div> -->
+  <img class="h-90 w-full" src="/img/footer.png" alt="Footer Image" />
 </template>
 
 <script setup></script>
