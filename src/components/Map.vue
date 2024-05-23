268174f012eb77e8fe822b4def561245f0c6e3b3
Sabrina <chiawenli.1219@gmail.com>
2024-05-23 20:39:22 +0800

    [MODIFY]

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="map-container bg-gray-100">
-    <div class="flex justify-center items-center px-4 py-8">
-      <!-- Left side: Image -->
-      <div class="flex-shrink-0">
+  <!-- <div class="map-container bg-gray-100">
+    <div class="flex justify-center items-center px-4 py-8"> -->
+  <!-- Left side: Image -->
+  <!-- <div class="flex-shrink-0">
         <img class="h-80 w-auto" src="/img/banner.png" alt="Hero Image" />
-      </div>
+      </div> -->
 
-      <!-- Right side: Title -->
-      <div class="ml-4">
+  <!-- Right side: Title -->
+  <!-- <div class="ml-4">
         <h1 class="text-2xl font-bold text-black">地圖</h1>
         <p class="text-lg text-gray-800">地圖</p>
       </div>
     </div>
+  </div> -->
+  <div class="py-10">
+    <h2 class="text-left text-2xl">周邊地圖</h2>
+    <img class="h-150 w-full" src="/img/taichungstation.png" alt="Hero Image" />
   </div>
 </template>
 
