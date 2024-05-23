e9982cbfefa4b4799fc792f42450932cb22ce09a
Sabrina <chiawenli.1219@gmail.com>
2024-05-23 20:40:00 +0800

    [MODIFY]

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <NavBar></NavBar>
   <Hero></Hero>
   <Menu></Menu>
-  <Calendar></Calendar>
+  <FullCalendar></FullCalendar>
   <Map></Map>
   <Footer></Footer>
 </template>
@@ -14,6 +14,7 @@ import Menu from "./components/Menu.vue";
 import Calendar from "./components/Calendar.vue";
 import Map from "./components/Map.vue";
 import Footer from "./components/Footer.vue";
+import FullCalendar from "./components/FullCalendar.vue";
 </script>
 
 <style scoped>
