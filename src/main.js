97c5d5f6ee0a807eb8c090cf775671d54d5346c6
Sabrina <chiawenli.1219@gmail.com>
2024-05-25 11:23:34 +0800

    [ADD] icons

--- src/main.js
+++ src/main.js
@@ -10,10 +10,14 @@ import { createVuetify } from "vuetify";
 // Element-plus imports
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app = createApp(App);
 const vuetify = createVuetify();
 app.use(vuetify);
 app.use(ElementPlus);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 // app.use(router);
 app.mount("#app");
