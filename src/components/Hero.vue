876c65c30363f9813e479fe0516b8f2864939a96
Sabrina <chiawenli.1219@gmail.com>
2024-05-27 22:55:31 +0800

    [MODIFY] banner rwd

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,29 +2,29 @@
   <div class="hero-container bg-orange-100">
     <div class="flex justify-center items-center px-4 py-8">
       <!-- Left side: Image -->
-      <div class="flex-shrink-0">
+      <div class="flex-shrink-0 hero-image">
         <img class="h-80 w-auto" src="/img/banner.png" alt="Hero Image" />
       </div>
 
       <!-- Right side: Title -->
-      <div class="ml-4">
+      <div class="ml-4 hide-on-small-screen">
         <h1 class="text-2xl font-bold text-black">資源不浪費</h1>
         <p class="text-lg text-gray-800">無家者的生活保障</p>
-        <!-- <button
-          class="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md shadow-lg hover:bg-blue-600"
-        >
-          表單
-        </button> -->
-        <DemandForm></DemandForm>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
-import DemandForm from "./DemandForm.vue";
-</script>
+<script setup></script>
 
 <style>
-/* Add custom styles here if needed */
+@media (max-width: 600px) {
+  .h-80 {
+    height: 7em;
+  }
+
+  .hide-on-small-screen {
+    display: none;
+  }
+}
 </style>
