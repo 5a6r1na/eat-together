00e751d19e4fa99af1bd0366ba1fc3eb9d9db2bd
Sabrina Li <chiawenli.1219@gmail.com>
2024-08-03 14:53:08 +0800

    [MODIFY] footer and introductio section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <NavBar></NavBar>
   <Hero></Hero>
-  <!-- <Menu></Menu> -->
+  <Introduction :class="{ open: isOpen }" class="introduction"></Introduction>
   <FullCalendar></FullCalendar>
   <Map></Map>
   <Footer></Footer>
@@ -10,23 +10,55 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
 import Hero from "./components/Hero.vue";
-import Menu from "./components/Menu.vue";
+import Introduction from "./components/Introduction.vue";
 import Map from "./components/Map.vue";
 import Footer from "./components/Footer.vue";
 import FullCalendar from "./components/FullCalendar.vue";
+
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const isOpen = ref(true);
+let lastScrollTop = 0;
+
+const handleScroll = () => {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down
+    isOpen.value = false;
+  } else {
+    // Scrolling up
+    isOpen.value = true;
+  }
+  lastScrollTop = Math.max(scrollTop, 0); // Ensure the lastScrollTop is non-negative
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.introduction {
+  width: 100%;
+  top: 0;
+  left: 0;
+  z-index: 2; /* Ensure the intro container is above the hero container */
+  transition: transform 1s ease-in-out, opacity 1s ease-in-out; /* Subtle transition for both transform and opacity */
+  transform: translateY(0);
+  opacity: 1;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.introduction.open {
+  transform: translateY(0);
+  opacity: 1;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.introduction:not(.open) {
+  transform: translateY(-100%);
+  opacity: 0;
 }
 </style>
