058163beabe825438a69f5c71dadf5fa4422e956
Sabrina Li <chiawenli.1219@gmail.com>
2024-08-03 14:52:26 +0800

    [ADD] introduction section with scroll effect

---
+++ src/components/Introduction.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="intro-container">
+    <div class="intro-background">
+      <div class="intro-text">
+        <p>
+          「赤貧者，不是社會的問題，而是為我們指出社會問題所在的人。透過深入貧者的視野，汲取他們的經驗，
+          將為全體人類找到一條不再貧窮的道路。」—— 台灣街角家關懷協會
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.intro-container {
+  /* position: fixed; */
+  height: 300px;
+  width: 100%;
+  top: 0;
+  left: 0;
+  z-index: 2;
+}
+
+.intro-background {
+  background-image: url("/img/streetpic.jpg");
+  background-size: cover;
+  background-position: center;
+  padding: 20px;
+  height: 100%;
+}
+
+.intro-text {
+  font-size: 16px;
+  text-align: center;
+  color: white;
+  background: rgba(
+    0,
+    0,
+    0,
+    0.5
+  ); /* Optional: Add a semi-transparent background for better text readability */
+  padding: 10px;
+  border-radius: 10px;
+  max-width: 90%; /* Optional: To ensure the text doesn't span the entire width */
+  margin: auto; /* Center the text container */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%; /* Ensures the text is centered vertically */
+}
+</style>
