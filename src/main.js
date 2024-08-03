980e499303a06bab9f29defa4cc709c2402622ef
Sabrina Li <chiawenli.1219@gmail.com>
2024-08-03 14:53:31 +0800

    [MODIFY] import pinina

--- src/main.js
+++ src/main.js
@@ -12,12 +12,20 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
+// Pinia import
+import { createPinia } from "pinia";
+
+// import router from "./router";
+
 import { db } from "./firebase";
 
 const app = createApp(App);
 const vuetify = createVuetify();
+const pinia = createPinia();
+
 app.use(vuetify);
 app.use(ElementPlus);
+app.use(pinia);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
